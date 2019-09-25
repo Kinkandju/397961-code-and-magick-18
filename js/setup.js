5895912dc3165d054ef1f51f42fce9f272a19f10
Kinkandju <ferretsphilosophy@gmail.com>
2019-09-25 20:57:15 +0300

    Исправление цветов в массиве с мантиями

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
 
@@ -24,18 +24,17 @@ var getRandomElement = function (arr) {
 for (var i = 0; i < WIZARDS_COUNT; i++) {
   wizards.push({
     name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
-    eyesColor: getRandomElement(WIZARD_EYES),
-    coatColor: getRandomElement(WIZARD_COATS)
+    coatColor: getRandomElement(WIZARD_COATS),
+    eyesColor: getRandomElement(WIZARD_EYES)
   });
 }
-
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
